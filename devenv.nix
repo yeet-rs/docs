aefa6f13cab41bf7fe478f5f8ad7bba549e198c0
timothebot <timoborer@gmail.com>
2026-03-20 14:49:18 +0100

    feat: :tada: Move docs to new repo, improve structure and add workflows

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    pkgs.mdbook
+    pkgs.mdbook-mermaid
+  ];
+
+  
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
